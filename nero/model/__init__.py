e62d7d098ef410760d3b33bdb19ad6bab40a2c80
kelechi-c <narutokelechi@gmail.com>
2026-07-26 05:50:48 +0100

    [add] init files

--- nero/model/__init__.py
+++ nero/model/__init__.py
@@ -2,7 +2,15 @@
 
 from nero.model.code_predictor import CodePredictor
 from nero.model.codec import CodecDecoder
+from nero.model.custom_voice import Qwen3CustomVoice
 from nero.model.talker import Talker
 from nero.model.tts import GenerationResult, Qwen3TTS
 
-__all__ = ["CodePredictor", "CodecDecoder", "GenerationResult", "Qwen3TTS", "Talker"]
+__all__ = [
+    "CodePredictor",
+    "CodecDecoder",
+    "GenerationResult",
+    "Qwen3CustomVoice",
+    "Qwen3TTS",
+    "Talker",
+]
---
+++ tests/__init__.py
@@ -0,0 +1,1 @@
+"""Whistle test suite."""
